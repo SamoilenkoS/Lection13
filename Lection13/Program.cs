7496ec041f22f3d4310204b4a013e1d80192f34a
Sviatoslav Samoilenko <Sviatoslav_Samoilenko@epam.com>
2021-12-11 10:04:29 +0200

    Add project files.

---
+++ Lection13/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lection13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
