99af7d66a1e27b9656d82a03ca1bc6afed54dfca
Sviatoslav Samoilenko <Sviatoslav_Samoilenko@epam.com>
2021-12-11 10:06:57 +0200

    Implemented max for array

--- Lection13/Program.cs
+++ Lection13/Program.cs
@@ -4,6 +4,20 @@ namespace Lection13
 {
     class Program
     {
+        static int Max(int[] array)
+        {
+            int max = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if(array[i] > max)
+                {
+                    max = array[i];
+                }
+            }
+
+            return max;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
